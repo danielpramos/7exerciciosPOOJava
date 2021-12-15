90369668bab7dfe3d4cc3ff3a948e7f043adabbb
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:00:22 -0300

    Exercicio 1 ClienteTeste POO Java

---
+++ ClienteTeste1.java
@@ -0,0 +1,11 @@
+package PacotePOOJava;
+
+public class ClienteTeste1 
+{
+
+	public static void main(String[] args) 
+	{
+		Cliente1 Cliente1 = new Cliente1 ("David", 27 , 1.80);
+        Cliente1.imprimirInfo();
+    }
+}
