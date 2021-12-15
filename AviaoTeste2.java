e75f0917874d3d7b40942505aa9c33f10a5c5276
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:03:00 -0300

    Exercicio 2 AviãoTeste POO Java

---
+++ AviaoTeste2.java
@@ -0,0 +1,14 @@
+package PacotePOOJava;
+
+public class AviaoTeste2 
+{
+
+	public static void main(String[] args) 
+	{
+		Aviao2 aviao1 = new Aviao2("Boeing", 43.8, 189);
+		aviao1.setligar();
+		aviao1.imprimirInfo();
+
+	}
+
+}
