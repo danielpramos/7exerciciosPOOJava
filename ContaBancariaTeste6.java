42c9745eed9140c843aa0d25e8c4db2580563258
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:26:48 -0300

    Exercicio 6 ContaBancariaTeste POO Java

---
+++ ContaBancariaTeste6.java
@@ -0,0 +1,13 @@
+package PacotePOOJava;
+
+public class ContaBancariaTeste6 
+{
+
+	public static void main(String[] args) 
+	{
+		ContaBancaria6 conta1 = new ContaBancaria6("João", "Bradesco", 12345.67, 337);
+		conta1.imprimirInfo();
+
+	}
+
+}
