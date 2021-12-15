d1ca9add63ecff6febdc2afb5973ce5cce4c1ccb
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:28:52 -0300

    Exercicio 7 PacienteTeste POO Java

---
+++ PacienteTeste7.java
@@ -0,0 +1,13 @@
+package PacotePOOJava;
+
+public class PacienteTeste7 
+{
+
+	public static void main(String[] args) 
+	{
+		Paciente7 paciente1 = new Paciente7("Felipe Pereira", 4575, "Apêndicite", 473475847);
+		paciente1.imprimirInfo();
+
+	}
+
+}
