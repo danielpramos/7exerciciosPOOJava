0aa4516ac79fac24818f9064bd8af8dc816616ab
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:21:16 -0300

    Exercicio 4 FuncionarioTeste POO Java

---
+++ FuncionarioTeste4.java
@@ -0,0 +1,12 @@
+package PacotePOOJava;
+
+public class FuncionarioTeste4 
+{
+
+	public static void main(String[] args) 
+	{
+		Funcionario4 funcionario = new Funcionario4("Daniel Pinheiro", "Itaú", "Desenvolvedor Java Jr", "1234", 4.999);
+		funcionario.imprimirInfo();
+	}
+
+}
