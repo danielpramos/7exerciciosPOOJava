d20407375c653c26b4016ab96689c22598025c0a
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:18:16 -0300

    Exercicio 3 ProdutoEletronicoTeste POO Java

---
+++ ProdutoEletronicoTeste3.java
@@ -0,0 +1,12 @@
+package PacotePOOJava;
+
+public class ProdutoEletronicoTeste3 {
+
+	public static void main(String[] args) {
+		
+		ProdutoEletronico3 celular = new ProdutoEletronico3("Samsung", "S7 Edge", "Dourada");
+		celular.imprimirInfo();
+
+	}
+
+}
