ab513546c96ae8cdb2773bc3d9e3769cb237b8d3
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-14 23:23:09 -0300

    Exercicio 5 PatineteTeste POO Java

---
+++ PatineteTeste5.java
@@ -0,0 +1,13 @@
+package PacotePOOJava;
+
+public class PatineteTeste5 
+{
+
+	public static void main(String[] args) 
+	{
+		Patinete5 patinete = new Patinete5("Xaomi", "Essential", "elétrico");
+		patinete.imprimirInfo();
+
+	}
+
+}
